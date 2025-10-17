bdaeae8210162c13d2de0872723ab955478ff241
FernandaSantamaria <fersantama05@gmail.com>
2025-10-17 10:29:05 -0600

    Definir clase albums

---
+++ app/src/main/java/com/example/fsantamariamusicapp/models/Album.kt
@@ -0,0 +1,9 @@
+package com.example.fsantamariamusicapp.models
+
+class Album(
+    val title : String,
+    val artist : String,
+    val description : String,
+    val image: String,
+    val id : String
+)
